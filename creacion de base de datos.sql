fa3be96d2f04d367245d3b01f61fdd2bd53f2d55
JUANSPORTELA <juansebastianportelaprado@gimnasiocalibio.edu.co>
2023-03-02 12:02:27 -0500

    creacion de base de datos

---
+++ creacion de base de datos.sql
@@ -0,0 +1,13 @@
+create database noveno;
+drop database noveno;
+create database calibio;
+create table estudiante(
+nombre varchar (50)not null;
+apellido varchar (50)not null;
+id numeric(10) not full primary key;
+insert into estudiante values("santiago", "gonzalez",3214569874, "mariana", "garcia", 31254567894,"juan","portela",56465434,"isabela";"delgado",155555,
+"juan", "herrera",1564115);
+select * from estudiante
+
+
+);
